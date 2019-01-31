2a22fecd15187e0271221c71826dd2ecc3cfe234
bspriggs <ben@sprico.com>
2019-01-30 21:07:22 -0800

    Move format to instance method

--- parsed_module.py
+++ parsed_module.py
@@ -4,10 +4,91 @@ from utils import flatten, append
 from typing import Iterable, Tuple
 
 class Module:
-    def __init__(self, imports: Iterable[Tuple[str, str]], statements, exports):
+    def __init__(self, imports: Iterable[Tuple[str, str]], statements: Iterable[asttypes.Node], exports):
         self.imports = imports
         self.statements = statements
         self.exports = exports
+
+    def formatted_imports(self) -> Iterable[str]:
+        if not self.imports:
+            return
+
+        for import_statement in self.imports:
+            source, name = import_statement
+            yield ("import * as {} from {}".format(name.value, source.value))
+
+    def formatted_statements(self) -> Iterable[str]:
+        if not self.statements:
+            return
+        yield from ([str(statement) for statement in self.statements])
+
+    def formatted_exports(s) -> Iterable[str]:
+        if not s.exports:
+            return
+
+        if not hasattr(s.exports, 'expr'):
+            """
+            ```js
+            define({
+                color: "black",
+                size: "unisize"
+            });
+            ```
+            """
+            yield ('export default {}'.format(s.exports))
+        elif isinstance(s.exports.expr, asttypes.Identifier):
+            """
+            ```js
+            define(..., function () {
+                var name = ...;
+                return name;
+            });
+            ```
+            """
+            yield ('export default {}'.format(s.exports.expr.value))
+        elif isinstance(s.exports.expr, asttypes.FuncExpr):
+            """
+            ```js
+            define(..., function () {
+                var name = ...;
+                return function () {
+                    ...;
+                };
+            });
+            ```
+            or:
+            ```js
+            define(..., function () {
+                var name = ...;
+                return function NamedFunction() {
+                    ...;
+                };
+            });
+            ```
+            """
+            name = s.exports.expr.identifier
+            args = ', '.join(p.value for p in s.exports.expr.parameters)
+            body = (str(line) for line in s.exports.expr.elements)
+            yield ('export default function {}({}) {{'.format(name, args))
+            yield from body
+            yield '}'
+        elif isinstance(s.exports.expr, asttypes.Object):
+            """
+            define(..., function () {
+                return {
+                    ...;
+                };
+            });
+            """
+            props = s.exports.expr.properties
+            as_props = ', '.join('{} as {}'.format(p.left.value, p.right.value) for p in props)
+            yield ('export {{ {} }}'.format(as_props))
+
+    def formatted(self) -> Iterable[str]:
+        yield from self.formatted_imports()
+        yield from self.formatted_statements()
+        yield from self.formatted_exports()
+
 
 def is_return(s: asttypes.Node) -> bool:
     return isinstance(s, asttypes.Return)
@@ -108,6 +189,7 @@ def module_from_statement(statement: asttypes.Node) -> Module:
             statements=statements, 
             exports=rstatement)
 
+
 def module(tree: asttypes.ES5Program) -> Iterable[Module]:
     if not isinstance(tree, asttypes.ES5Program):
         raise Exception("not a program")
@@ -118,75 +200,7 @@ def module(tree: asttypes.ES5Program) -> Iterable[Module]:
         else:
             yield from module_from_statement(statement)
 
-def format_module(s: Module) -> Iterable[str]:
-    if s.imports:
-        for import_statement in s.imports:
-            source, name = import_statement
-            yield ("import * as {} from {}".format(name.value, source.value))
-
-    if s.statements:
-        yield from ([str(statement) for statement in s.statements])
-
-    # returns, if any
-    if s.exports:
-        if not hasattr(s.exports, 'expr'):
-            """
-            ```js
-            define({
-                color: "black",
-                size: "unisize"
-            });
-            ```
-            """
-            yield ('export default {}'.format(s.exports))
-        elif isinstance(s.exports.expr, asttypes.Identifier):
-            """
-            ```js
-            define(..., function () {
-                var name = ...;
-                return name;
-            });
-            ```
-            """
-            yield ('export default {}'.format(s.exports.expr.value))
-        elif isinstance(s.exports.expr, asttypes.FuncExpr):
-            """
-            ```js
-            define(..., function () {
-                var name = ...;
-                return function () {
-                    ...;
-                };
-            });
-            ```
-            or:
-            ```js
-            define(..., function () {
-                var name = ...;
-                return function NamedFunction() {
-                    ...;
-                };
-            });
-            ```
-            """
-            name = s.exports.expr.identifier
-            args = ', '.join(p.value for p in s.exports.expr.parameters)
-            body = (str(line) for line in s.exports.expr.elements)
-            yield ('export default function {}({}) {{'.format(name, args))
-            yield from body
-            yield '}'
-        elif isinstance(s.exports.expr, asttypes.Object):
-            """
-            define(..., function () {
-                return {
-                    ...;
-                };
-            });
-            """
-            props = s.exports.expr.properties
-            as_props = ', '.join('{} as {}'.format(p.left.value, p.right.value) for p in props)
-            yield ('export {{ {} }}'.format(as_props))
 
 def formatted_module(tree: asttypes.ES5Program) -> Iterable[str]:
-    return flatten(format_module(m) for m in module(tree))
+    return flatten(m.formatted() for m in module(tree))
 
